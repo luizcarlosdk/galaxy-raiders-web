e227a09c4d7a01de05aa3f353f79a7685bc9d145
Luiz <luizcarlosdk@hotmail.com>
2023-07-04 17:55:47 -0300

    Add menu logic
    Signed-off-by: Luiz <luizcarlosdk@hotmail.com>

---
+++ components/Leaderboard.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="leaderboard">
+
+    <div class="players">
+      <p class="textDecorated">Primeiro lugar</p>
+      <p class="subText">Pontuação: {{ scores[0].score }}</p>
+      <p class="subText">Asteroides destruidos: {{ scores[0].asteroidsDestroyed }}</p>
+    </div>
+    <div class="players">
+      <p class="textDecorated">Segundo lugar</p>
+      <p class="subText">Pontuação: {{ scores[1].score }}</p>
+      <p class="subText">Asteroides destruidos: {{ scores[1].asteroidsDestroyed }}</p>
+    </div>
+    <div class="players">
+      <p class="textDecorated">Terceiro lugar</p>
+      <p class="subText">Pontuação: {{ scores[2].score }}</p>
+      <p class="subText">Asteroides destruidos: {{ scores[2].asteroidsDestroyed }}</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import leaderboard from "../assets/leaderboard.json"
+
+export default {
+
+  teste() {
+  },
+
+  data: () => ({
+    scores: leaderboard
+
+  }),
+
+}
+
+
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Anton&family=Dancing+Script:wght@400;700&family=Lobster&display=swap');
+
+.textDecorated {
+  font-family: 'Anton', sans-serif;
+  font-size: 30px;
+  color:pink
+
+}
+
+.subText {
+  font-family: 'Lobster', cursive;
+  font-size: 15px;
+  color: cyan
+}
+
+#leaderboard {
+  width: 40%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.players {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 40%;
+  height: 40%;
+  opacity: 80%;
+}</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,47 @@
   <div id="canvas">
     <div id="deep-space" />
     <div id="space-field">
-      <SpaceObject id="spaceship" class="spaceship" :data="spaceField.ship" resolution="2" />
+      <Leaderboard v-if="showLearderboard" />
+      <div id="menu" v-show="menuOpen">
+        <img src="~/assets/play.png" id="play" v-on:click="playGame" v-if="!gameStarted">
+        <img src="~/assets/leaderboard.png" v-on:click="showLeader" width="80%">
+        <img src="~/assets/quit.png" width="45%" v-on:click="quitGame">
+      </div>
+      <SpaceObject id="spaceship" class="spaceship" :data="spaceField.ship" resolution="2" v-if="gameStarted" />
 
-      <SpaceObject class="asteroid" :data="asteroid" resolution="2"
-                  :key="asteroid.center"
-                  v-for="asteroid in spaceField.asteroids" />
+      <SpaceObject class="asteroid" :data="asteroid" resolution="2" :key="asteroid.center"
+        v-for="asteroid in spaceField.asteroids" />
 
-      <SpaceObject class="missile" :data="missile" resolution="2"
-                  :key="missile.center"
-                  v-for="missile in spaceField.missiles" />
+      <SpaceObject class="missile" :data="missile" resolution="2" :key="missile.center"
+        v-for="missile in spaceField.missiles" />
 
-      <SpaceObject class="explosion" :data="explosion" resolution="2"
-                  :key="explosion.center"
-                  v-for="explosion in spaceField.explosions" />
+      <SpaceObject class="explosion" :data="explosion" resolution="2" :key="explosion.center"
+        v-for="explosion in spaceField.explosions" />
     </div>
   </div>
 </template>
 
 <script setup>
+
+import Leaderboard from '~~/components/Leaderboard.vue';
+
+let menuOpen = 1;
+let gameStarted = 0
+let showLearderboard = false
+
+const playGame = () => {
+  menuOpen = 0;
+  gameStarted = 1;
+}
+
+const quitGame = () => {
+  $post("/quit")
+};
+
+const showLeader = () => {
+  showLearderboard = showLearderboard === 1 ? 0 : 1;
+}
+
 const {
   data: spaceField,
   refresh: updateSpaceField
@@ -35,21 +58,44 @@ onMounted(() => {
       "Space": "LAUNCH_MISSILE",
       "Escape": "PAUSE_GAME",
     };
-
     const command = keyToCommand[event.code];
 
     // Ignore if invalid key was pressed
     if (command === undefined) return;
 
-    console.log(`Triggering command: ${command}`);
-    await $post("/ship/commands", { command })
+      if (command === 'PAUSE_GAME') {
+          menuOpen = menuOpen === 1 ? 0 : 1;
+          
+      }
+      console.log(`Triggering command: ${command}`);
+      await $post("/ship/commands", { command })
+
   });
 
-  window.setInterval(updateSpaceField, 1000);
+  window.setInterval(updateSpaceField, 16);
 })
 </script>
 
 <style>
+#play {
+  width: 50%;
+}
+
+#menu {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 40%;
+  height: 40%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  opacity: 80%;
+}
+
+
 #canvas {
   height: calc(100vh - 4rem);
   width: calc(100vw - 4rem);
@@ -70,9 +116,11 @@ onMounted(() => {
   0% {
     transform: translate(1px);
   }
+
   50% {
     transform: translate(-1px);
   }
+
   100% {
     transform: translate(1px);
   }
@@ -110,6 +158,6 @@ onMounted(() => {
 }
 
 .explosion {
-  background-image: url("~/assets/explosion.png");
+  background-image: url("~/assets/explosion.gif");
 }
 </style>
